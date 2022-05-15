0cae966c721a2c6e5c82f74d4f471cc22161de64
Robin van Maasakker <robin@qubiqx.com>
2022-05-15 10:24:24 +0200

    Update

--- src/Classes/PageRouteHandler.php
+++ src/Classes/PageRouteHandler.php
@@ -19,6 +19,9 @@ class PageRouteHandler
             foreach ($slugParts as $slugPart) {
                 $page = Page::publicShowable()->where('slug->' . App::getLocale(), $slugPart)->where('parent_page_id', $parentPageId)->where('is_home', 0)->first();
                 $parentPageId = $page?->id;
+                if(!$page){
+                    return 'pageNotFound';
+                }
             }
         } else {
             $page = Page::publicShowable()->where('is_home', 1)->first();
