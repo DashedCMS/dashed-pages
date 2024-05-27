22bd1e7b5601c3cd7eab27edaac3f00ac13893e9
Robin van Maasakker <robin@qubiqx.com>
2024-05-27 15:09:53 +0200

    Update

--- src/Filament/Resources/PageResource/Pages/EditPage.php
+++ src/Filament/Resources/PageResource/Pages/EditPage.php
@@ -8,11 +8,11 @@ use Dashed\DashedPages\Filament\Resources\PageResource;
 use Dashed\DashedPages\Models\Page;
 use Filament\Resources\Pages\EditRecord;
 use Filament\Resources\Pages\EditRecord\Concerns\Translatable;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Str;
 
 class EditPage extends EditRecord
 {
-    use Translatable;
     use HasEditableCMSActions;
 
     protected static string $resource = PageResource::class;
@@ -20,18 +20,5 @@ class EditPage extends EditRecord
     protected function getActions(): array
     {
         return self::CMSActions();
-    }
-
-    protected function mutateFormDataBeforeSave(array $data): array
-    {
-        $data['slug'] = Str::slug($data['slug'] ?: $data['name']);
-
-        while (Page::where('id', '!=', $this->record->id)->where('slug->' . $this->activeLocale, $data['slug'])->count()) {
-            $data['slug'] .= Str::random(1);
-        }
-
-        $data['site_ids'] = $data['site_ids'] ?? [Sites::getFirstSite()['id']];
-
-        return $data;
     }
 }
