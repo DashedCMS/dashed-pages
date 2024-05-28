d018e2c1f18afbc6b7c322cafd8e92ac6a2e7911
Robinvm <Robinvm@users.noreply.github.com>
2024-05-28 06:51:41 +0000

    Fix styling

--- src/Filament/Resources/PageResource/Pages/CreatePage.php
+++ src/Filament/Resources/PageResource/Pages/CreatePage.php
@@ -2,14 +2,9 @@
 
 namespace Dashed\DashedPages\Filament\Resources\PageResource\Pages;
 
-use Dashed\DashedCore\Classes\Sites;
 use Dashed\DashedCore\Filament\Concerns\HasCreatableCMSActions;
 use Dashed\DashedPages\Filament\Resources\PageResource;
-use Dashed\DashedPages\Models\Page;
-use Filament\Actions\LocaleSwitcher;
 use Filament\Resources\Pages\CreateRecord;
-use Filament\Resources\Pages\CreateRecord\Concerns\Translatable;
-use Illuminate\Support\Str;
 
 class CreatePage extends CreateRecord
 {
