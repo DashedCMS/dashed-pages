0695dc079a5e0f2f9ce0647a58fed73f89923cbb
Robin van Maasakker <robin@qubiqx.com>
2024-05-28 08:51:26 +0200

    Update

--- src/Filament/Resources/PageResource/Pages/CreatePage.php
+++ src/Filament/Resources/PageResource/Pages/CreatePage.php
@@ -3,6 +3,7 @@
 namespace Dashed\DashedPages\Filament\Resources\PageResource\Pages;
 
 use Dashed\DashedCore\Classes\Sites;
+use Dashed\DashedCore\Filament\Concerns\HasCreatableCMSActions;
 use Dashed\DashedPages\Filament\Resources\PageResource;
 use Dashed\DashedPages\Models\Page;
 use Filament\Actions\LocaleSwitcher;
@@ -12,27 +13,12 @@ use Illuminate\Support\Str;
 
 class CreatePage extends CreateRecord
 {
-    use Translatable;
+    use HasCreatableCMSActions;
 
     protected static string $resource = PageResource::class;
 
     protected function getActions(): array
     {
-        return [
-            LocaleSwitcher::make(),
-        ];
-    }
-
-    protected function mutateFormDataBeforeCreate(array $data): array
-    {
-        $data['slug'] = Str::slug($data['slug'] ?: $data['name']);
-
-        while (Page::where('slug->' . $this->activeLocale, $data['slug'])->count()) {
-            $data['slug'] .= Str::random(1);
-        }
-
-        $data['site_ids'] = $data['site_ids'] ?? [Sites::getFirstSite()['id']];
-
-        return $data;
+        return self::CMSActions();
     }
 }
